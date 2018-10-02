21464697193c2a1bc2f60d0636a3f0e6fd12eb49
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-10-02 01:31:48 +0000

    Atualizando

---
+++ src/main/java/blockly/TestandoTesteALT.java
@@ -0,0 +1,28 @@
+package blockly;
+
+import cronapi.*;
+import cronapi.rest.security.CronappSecurity;
+import java.util.concurrent.Callable;
+
+@CronapiMetaData(type = "blockly")
+@CronappSecurity
+public class TestandoTesteALT {
+
+	public static final int TIMEOUT = 300;
+
+	/**
+	 *
+	 * @return Var
+	 */
+	// Testando
+	public static Var Executar() throws Exception {
+		return new Callable<Var>() {
+
+			public Var call() throws Exception {
+				System.out.println(Var.valueOf("Testando salvamento de Blcos").getObjectAsString());
+				return Var.VAR_NULL;
+			}
+		}.call();
+	}
+
+}
